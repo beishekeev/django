6a284cbaf2cb2845b0a79c525d2ddeaa897f0a10
beishekeev <beihekeevb140800@gmail.com>
2023-08-30 22:51:54 +0600

    first commit

--- TODODJANGO/settings.py
+++ TODODJANGO/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'TODODJANGO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'DIRS': ["templates", BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from tasks.models import Task
+
+
+def get_list(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        return render(request, 'index.html', {"tasks": tasks})
