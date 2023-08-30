32837c6b9b14e8e89d3deb456d639b9207cde382
beishekeev <beihekeevb140800@gmail.com>
2023-08-30 15:49:59 +0600

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.db import models
+# Create your models here.
+class Task(models.Model):
+    CHOICE_TYPE = (
+        ('work', 'Work'),
+        ('home', 'Home'),
+        ('edu', 'Edu')
+    )
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_created=True)
+    type = models.CharField(max_length=25, choices=CHOICE_TYPE)
+
+    def __str__(self):
+        return self.title
