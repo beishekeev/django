5b95d6248c89e6f68e27e42a6db27d9c8bc69488
beishekeev <beihekeevb140800@gmail.com>
2023-09-01 17:28:52 +0600

    bek

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,10 @@
+
 from django.urls import path
-from tasks.views import get_list
+from tasks.views import get_list, create_task, update_task, delete_task
 
 urlpatterns = [
-    path('', get_list, name='list')
+    path('', get_list, name='index'),
+    path('create', create_task, name='create'),
+    path('update/<int:id>', update_task, name='update'),
+    path('delete/<int:id>', delete_task, name='delete')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -9,3 +9,37 @@ def get_list(request):
     if request.method == 'GET':
         tasks = Task.objects.all()
         return render(request, 'index.html', {"tasks": tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        created_at = request.POST.get('type')
+        type = request.POST.get('type')
+        task = Task.objects.create(title=title, description=description, created_at=created_at, type=type)
+        return redirect('index')
+    return render(request, 'index.html')
+
+def update_task(request, id):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        created_at = request.POST.get('created_at')
+        type = request.POST.get('type')
+        task = Task.objects.get(id=id)
+        task.title = title
+        task.description = description
+        task.created_at = created_at
+        task.type = type
+        task.save()
+        return redirect('index')
+    return render(request, 'update.html')
+
+
+def delete_task(request, id):
+    if request.method =='POST':
+        task = Task.objects.get(id=id)
+        task.delete()
+        return redirect('index')
+    return render(request, 'index.html')
+
